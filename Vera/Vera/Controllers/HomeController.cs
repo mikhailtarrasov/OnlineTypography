516a8a4cc22ae56c71f5a7dba5e0dd7dffe48048
mozard742 <mikhailtarrasov@gmail.com>
2016-12-05 11:11:34 +0500

    Выпадающее меню с форматами, без отображения цены за клей

--- Vera/Vera/Controllers/HomeController.cs
+++ Vera/Vera/Controllers/HomeController.cs
@@ -16,42 +16,51 @@ namespace Vera.Controllers
 
         public ActionResult Index()
         {
-            DatabaseContext Context = new DatabaseContext();
-
-            Context.Currencies.Add(new Currency() { Name = "RUB", Rate = 1m });
-            Context.Currencies.Add(new Currency() { Name = "EUR", Rate = 68.47m });
-
-            Context.SaveChanges();
-
-            var rubCurrency = Context.Currencies.FirstOrDefault(x => x.Name == "RUB");
 
             /*--------------------------------Заполняем БД----------------------------------*/
+            //FillInTheDatabase();
+            /*--------------------------------Заполняем БД----------------------------------*/
+
+            var context = new DatabaseContext();
+
+            ViewBag.Format = new SelectList(context.Formats, "Id", "Name");
+            
+            return View();
+        }
+
+
+
+        public void FillInTheDatabase()
+        {
+            DatabaseContext context = new DatabaseContext();
+
+            context.Currencies.Add(new Currency() { Name = "RUB", Rate = 1m });
+            context.Currencies.Add(new Currency() { Name = "EUR", Rate = 68.47m });
+
+            context.SaveChanges();
+
+            var rubCurrency = context.Currencies.FirstOrDefault(x => x.Name == "RUB");
+
             decimal cost = 10.24m;
             double area = 999949;
-            for (int i = 0; i < 11; i++)    
+            for (int i = 0; i < 11; i++)
             {
-                Context.Prices.Add(new Price() { Cost = cost, Currency = rubCurrency });   
-                Context.Formats.Add(new Format() { Name = "A" + i, Area = area });         
-                Context.SaveChanges();
+                context.Prices.Add(new Price() { Cost = cost, Currency = rubCurrency });
+                context.Formats.Add(new Format() { Name = "A" + i, Area = area });
+                context.SaveChanges();
 
-                Context.GluePrices.Add(new GluePrice()
+                context.GluePrices.Add(new GluePrice()
                 {
-                    Format = Context.Formats.FirstOrDefault(x => x.Name == "A" + i),
-                    Price = Context.Prices.FirstOrDefault(x => x.Cost == cost)
+                    Format = context.Formats.FirstOrDefault(x => x.Name == "A" + i),
+                    Price = context.Prices.FirstOrDefault(x => x.Cost == cost)
                 });
 
                 cost /= 2;
                 area /= 2;
             }
-            Context.SaveChanges();
-            /*--------------------------------Заполняем БД----------------------------------*/
-
-
-            //IEnumerable<Currency> currencies = Context.Currencies;
-            //ViewBag.Currencies = currencies;
-
-            return View();
+            context.SaveChanges();
         }
-
     }
+
+   
 }
