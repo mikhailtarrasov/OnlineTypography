201d30f24ac4ec7abcc4d99f2190f4943aa81df3
mozard742 <mikhailtarrasov@gmail.com>
2017-02-13 18:59:40 +0500

    Add all untracked files

---
+++ Vera/Vera/Migrations/201612182119495_0.3_Add_Sewing.cs
@@ -0,0 +1,35 @@
+namespace Vera.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _03_Add_Sewing : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Sewings",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FormingType_Id = c.Int(),
+                        Price_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.FormingTypes", t => t.FormingType_Id)
+                .ForeignKey("dbo.Prices", t => t.Price_Id)
+                .Index(t => t.FormingType_Id)
+                .Index(t => t.Price_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Sewings", "Price_Id", "dbo.Prices");
+            DropForeignKey("dbo.Sewings", "FormingType_Id", "dbo.FormingTypes");
+            DropIndex("dbo.Sewings", new[] { "Price_Id" });
+            DropIndex("dbo.Sewings", new[] { "FormingType_Id" });
+            DropTable("dbo.Sewings");
+        }
+    }
+}
---
+++ Vera/Vera/Scripts/myScript.js
@@ -0,0 +1,49 @@
+﻿
+//$(function () {
+//    $('#Format').change(function () {
+//        // получаем выбранный id
+//        var id = $(this).val();
+//        console.log(id);    // -
+
+//        console.log(b);
+//        $.ajax({
+//            type: 'POST',
+//            url: 'SetGluePrice/' + id,
+//            success: function (data) {
+//                $('#price').text(data);// заменяем содержимое присланным частичным представлением
+//            }
+//        });
+//    });
+//});
+
+//$(function() {
+//    $('#FormingType').change(function() {
+//        var id = $(this).val();
+        
+//        $.ajax({
+//            type: 'POST',
+//            url: '@Url.Action("FormingType")/' + id,
+//            success: function(data) {
+//                $('#formingTypeBlock').replaceWith(data); // заменяем содержимое присланным частичным представлением
+//            }
+//        });
+//    });
+//});
+
+//$(function () {
+//    $('#tetr').keyup(function () {
+//        var tetrCount = $('#tetr').val();
+//        var id = $('#Format').val();
+//        console.log(tetrCount);
+//        console.log(id);
+//        $.ajax({
+//            type: 'POST',
+//            url: '@Url.Action("SetSewingPrice")/' + id + '/' + tetrCount,
+//            success: function (data) {
+//                console.log(data);
+//                $('#blockPrice').empty();
+//                $('#blockPrice').text(data);
+//            }
+//        });
+//    });
+//});
