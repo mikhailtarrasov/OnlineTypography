0ed26d7cb68039940ce953a595f768344ab72c56
Михаил <mikhailtarrasov@gmail.com>
2017-03-25 17:32:22 +0500

    Новый функционал в модуле инвертаризации Появились такие операции над материалом, как: - Просмотр деталей; - Редактирование данных; - Удаление материала.

--- Vera/Vera/Controllers/InventoryController.cs
+++ Vera/Vera/Controllers/InventoryController.cs
@@ -17,6 +17,21 @@ namespace Vera.Controllers
             return View(db.Materials.ToList());
         }
 
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
         // GET: Inventory/Create
         public ActionResult Create()
         {
@@ -46,6 +61,71 @@ namespace Vera.Controllers
                 return RedirectToAction("Index");
             }
             return View(material);
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            ViewBag.Currencies = new SelectList(db.Currencies, "Id", "Name");
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Inventory/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,Type,Balance")] Material material, int currencyId)
+        {
+            if (ModelState.IsValid)
+            {
+                var price = db.Prices.Find(material.Price.Id);
+                price.Cost = material.Price.Cost;
+                price.Currency = db.Currencies.Find(currencyId);
+
+                material.Price = price;
+                db.Entry(material).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(material);
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Material material = db.Materials.Find(id);
+            Price materialPrice = db.Prices.Find(material.Price.Id);
+            db.Prices.Remove(materialPrice);
+            db.Materials.Remove(material);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
